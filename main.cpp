77ea52bf59162bbc265958d4d33400c7a8a6e55a
nikusick <91880536+nikusick@users.noreply.github.com>
2022-09-25 23:39:13 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
-#include <algorithm>
-#include <numeric>
+#include <iterator>
 
-
-int main(int argc, char** argv) {
-
-    std::ifstream fin(argv[1]);
-    std::ofstream fout(argv[2]);
-
+int main() {
+    int N, num;
+    std::cin >> N;
     std::vector<int> nums;
-    int num;
-    int N;
-
-    fin >> N;
-    while (fin >> num) {
+    for (int i = 0; i < N; ++i) {
+        std::cin >> num;
         nums.push_back(num);
     }
+    auto maxElIt = std::max_element(nums.begin(), nums.end());
 
-    std::sort(nums.begin(), nums.end());
-    auto opponentSum = std::accumulate(nums.begin(), nums.begin() + N / 2, 0);
-    auto ownSum = std::accumulate(nums.begin() + N / 2, nums.end(), 0);
+    auto is_even = [](int i){ return i % 2 == 0; };
+    auto elForDelIt = std::find_if(maxElIt + 1, nums.end(), is_even);
 
-    fout << ownSum - opponentSum;
-
-    fin.close();
-    fout.close();
+    auto index = std::distance(nums.begin(), elForDelIt);
+    if (elForDelIt != nums.end()) {
+        std::cout << "Позиция элемента: " << index << std::endl;
+        std::cout << "Значение элемента: " << *elForDelIt << std::endl;
+        nums.erase(elForDelIt);
+        std::cout << "Массив после удаления элемента: ";
+        std::copy(nums.begin(), nums.end(), std::ostream_iterator<int>(std::cout, " "));
+    }
+    else {
+        std::cout << "Такого элемента нет!";
+    }
     return 0;
 }
